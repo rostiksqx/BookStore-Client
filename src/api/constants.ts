2a9dc6e419adc0ac1eabcdc9afbf57bc8d589cd9
Rostyslav <rostikboss2005@gmail.com>
2024-07-05 19:55:50 +0300

    constants

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-export const url = "http://127.0.0.1:8080/api";
+export const url = "https://single-dulce-rostiksqx-cc3a113d.koyeb.app/api";
 export const KEY = process.env.RESEND_API_KEY;
 export const OWNER_EMAIL = process.env.OWNER_EMAIL;
 export const errorSearchParameterName = "error";
