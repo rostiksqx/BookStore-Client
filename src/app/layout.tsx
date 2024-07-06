2aa14e479fbc86dfc77b3289ff83fd394fac0161
Rostyslav <rostikboss2005@gmail.com>
2024-07-06 14:55:04 +0300

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import BookProvider from "../context/book-provider";
+import Head from "next/head";
 
 
 const crimson_text = Crimson_Text({
@@ -22,10 +23,58 @@ const dm_serif_display = DM_Serif_Display({
 
 export const metadata: Metadata = {
   title: "Book Store",
-  description: "A simple book store created with Next.js and ASP.NET Core",
+  description: "A book store where you can find your favorite books and buy them online.",
+  robots: "index, follow",
   icons: "favicon.svg",
+  openGraph: {
+    locale: "en",
+    type: "website",
+    title: "Book Store",
+    description: "A book store where you can find your favorite books and buy them online.",
+    url: "https://book-store-rostik.netlify.app",
+    siteName: "Book Store",
+    images: ["https://book-store-rostik.netlify.app/bookstore.webp"],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Book Store",
+    description: "A book store where you can find your favorite books and buy them online.",
+    site: "https://twitter.com/bookstoreshop",
+    creator: "https://twitter.com/bookstoreshop",
+    images: ["https://book-store-rostik.netlify.app/bookstore.webp"],
+  },
+  keywords: [
+    "book store",
+    "books",
+    "buy books",
+    "online book store",
+    "bookstore", "bookshop",
+    "bookshop online",
+    "bookstore online",
+    "buy books online",
+    "bookstore near me",
+    "bookshop near me"
+  ],
+  generator: "Book Store",
 };
 
+const schema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Book Store",
+  "url": "https://book-store-rostik.netlify.app",
+  "logo": "https://book-store-rostik.netlify.app/favicon.svg",
+  "description": "A book store where you can find your favorite books and buy them online.",
+  "sameAs": [
+    "https://www.linkedin.com/in/book-store",
+    "https://twitter.com/bookstoreshop"
+  ],
+  "location": {
+    "@type": "VirtualLocation",
+    "url": "https://book-store-rostik.netlify.app/contact-us",
+    "description": "Contact us for more information about our book store."
+  }
+};
 
 export default function RootLayout({
   children,
@@ -35,6 +84,18 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <link rel="canonical" href="https://book-store-rostik.netlify.app" />
+        <link rel="sitemap" href="/sitemap-index.xml" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Book Store" />
+        <meta property="og:image:type" content="image/png" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </Head>
       <body className={crimson_text.variable + ' ' + dm_serif_display.variable}>
         <BookProvider>
           <div className="flex flex-col min-h-[100dvh]">
